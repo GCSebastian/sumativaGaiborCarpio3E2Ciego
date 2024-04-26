f2230ce29ec72ddb924028c9c3e53e7c9ad0bd24
GCSebastian <juan.gaiborc.est@uets.edu.ec>
2024-04-26 16:23:25 -0500

    SubidoFinal
    No se conecta bien a la base de datos y no tiene notificaciones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { Database, object, ref } from '@angular/fire/database';
 
 
 
@@ -8,24 +9,30 @@ import { AlertController } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
   Temperatura: number = 0; // Inicializa la temperatura en cero
   showAlert: boolean = false; 
-  constructor(public alertController: AlertController) {}
+  // constructor(public alertController: AlertController) {}
+  constructor(public alertController: AlertController, private database:Database) {}
 
-  
+    
 
-  async checkTemperature() {
-    if (this.Temperatura > 40) {
-      const alert = await this.alertController.create({
-        header: '¡Atención!',
-        message: `La temperatura es ${this.Temperatura}°C`,
-        buttons: ['OK']
+    ngOnInit() {
+      const route = ref(this.database, /*direccion a donde ir dentro de la base de datos */);
+      object(route).subscribe(attributes => {
+        const valores_db = attributes.snapshot.val();
+        console.log(valores_db);//Imprimir valores obtenidos de la busqueda en la ruta 
       });
+    }
   
-      await alert.present();
+
+  checkTemperature() {
+    if (this.Temperatura > 40) {
+      this.showAlert = true; // Muestra la alerta si la temperatura es mayor a 40°C
     }
   }
+
 
   async dismissAlert() {
     this.showAlert = false; // Oculta la alerta al cerrarla
@@ -46,6 +53,4 @@ export class HomePage {
     console.log('Nuevo valor de temperatura:', event.detail.value);
     // Aquí puedes realizar acciones adicionales según el valor seleccionado
   }
-
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,26 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true
 };
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqqx5Av2yan28TPt_uISU6RX31V0H9ld0",
+  authDomain: "termometer-iot.firebaseapp.com",
+  projectId: "termometer-iot",
+  storageBucket: "termometer-iot.appspot.com",
+  messagingSenderId: "55341521437",
+  appId: "1:55341521437:web:5a1e610ca6eb81a49c84f4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 /*
  * For easier debugging in development mode, you can import the following file
